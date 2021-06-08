3bc661a9e35b57e8a233dbf69a92f2c98a9887b6
Paul Fell <swellpf@gmail.com>
2021-06-08 14:25:09 -0400

    Setup of packages and imported pkgs to index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {createStore, applyMiddleware} from 'redux';
+import {Provider} from 'react-redux';
+import thunk from 'redux-thunk';
+
 
 ReactDOM.render(
   <React.StrictMode>
