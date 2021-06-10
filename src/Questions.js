cfe75d5142d6b62257807d94780f211722006837
Paul Fell <swellpf@gmail.com>
2021-06-09 21:04:34 -0400

    Adding components for implementing React Router

---
+++ src/Questions.js
@@ -0,0 +1,15 @@
+import React from 'react';
+ 
+class Questions extends React.Component {
+  render() {
+    return <h1>Questions?</h1>
+    <h2>  If you have questions about registration, please contact:</h2>;
+    <h2> Dr. Andrew Cinoman at (239) 555-0297 or by email at: acinoman@university.edu</h2>
+
+    <p><b>Please note:</b>  All full-time students are required to register for a minimum of 12 credit
+        hours and no more than 18 credit hours.  
+    </p>
+  }
+}
+ 
+export default About;
