455021dd99e498303a4ca43b9875ea600474f2f1
Paul Fell <swellpf@gmail.com>
2021-06-09 21:13:55 -0400

    Adding elements to NavBar

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
 import React from 'react';
 import CoursesContainer from './containers/CoursesContainer';
 import StudentsContainer from './containers/StudentsContainer';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+import NavBar from './NavBar';
+import Welcome from './Welcome';
+import Questions from './Questions';
+import Login from './Login';
 
 class App extends React.Component {
   
 
   render() {
     return (
-      <div className = "App">
-        <CoursesContainer />
-
-        <StudentsContainer />
-      </div>
+      <Router>
+        <div className = "App">
+          <NavBar />
+            <Route exact path='/' component={Home}/>
+            <Route exact path='Welcome' component={Welcome}/>
+            <Route exact path='Questions' component={Questions}/>
+            <Route exact path='Login' component={Login}/>
+        </div>
+      </Router>
     );
   }
 }
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,49 +1,21 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom';
  
-const link = {
-  width: '100px',
-  padding: '12px',
-  margin: '0 6px 6px',
-  background: 'blue',
-  textDecoration: 'none',
-  color: 'white',
-}
- 
-class Navbar extends React.Component {
-  render() {
+const NavBar =() => {
     return (
-      <div>
+      <div className="navbar">
         <NavLink
           to="/"
-          /* set exact so it knows to only set activeStyle when route is deeply equal to link */
-          exact
-          /* add styling to Navlink */
-          style={link}
-          /* add prop for activeStyle */
-          activeStyle={{
-            background: 'darkblue'
-          }}
-        >Home</NavLink>
+        >Welcome</NavLink>
         <NavLink
           to="/about"
-          exact
-          style={link}
-          activeStyle={{
-            background: 'darkblue'
-          }}
-        >About</NavLink>
+        >Questions</NavLink>
         <NavLink
           to="/login"
-          exact
-          style={link}
-          activeStyle={{
-            background: 'darkblue'
-          }}
         >Login</NavLink>
       </div>
-    )
-  }
-}
+    );
+  };
+
  
 export default Navbar;
